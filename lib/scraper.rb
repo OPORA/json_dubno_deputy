d647a28b66baa293e011f8ea3bef1f72acdf1f2e
Beastie PC <gskondratenko@opora.org.ua>
2019-04-26 19:49:08 +0300

    Added phtoto and replace okrug

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class ScrapeMp
   def parser
     #DataMapper.auto_upgrade!
     #
-     csv_file = open('https://ckan.dubno-adm.rv.ua/dataset/7eb66fa9-33de-4d73-b5a7-df6846823f98/resource/49c25673-4422-4bd6-92e4-b458a0e40818/download/depytatu_2018.csv')
+     csv_file = open('https://ckan.dubno-adm.rv.ua/dataset/7eb66fa9-33de-4d73-b5a7-df6846823f98/resource/5f032ca4-a599-4b22-9f9e-b13d5de3e1d7/download/depytatu_2018.csv')
      csvmp = CSV.read(csv_file)
      csvmp.shift
      csvmp.each do |mp|
@@ -43,41 +43,43 @@ class ScrapeMp
   end
   def create_mps_1
     names = %w{Іванова Марія Петрівна}
-    People.first_or_create(
+    p = People.first_or_create(
         first_name: names[1],
         middle_name: names[2],
         last_name: names[0],
         full_name: names.join(' '),
         deputy_id: 35,
         okrug: 22,
-        photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
+        #photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
         faction: "Українське об'єднання патріотів - УКРОП",
         start_date: "2015-10-20",
         end_date:  nil,
         created_at: "9999-12-31",
         updated_at: "9999-12-31"
     )
+    p.update(photo_url: "http://dubno-adm.rv.ua/UserFiles/depytat/ivanovamp.jpg")
   end
   def create_mps_2
     names = %w{Момотюк Юрій Володимирович}
-    People.first_or_create(
+    p = People.first_or_create(
         first_name: names[1],
         middle_name: names[2],
         last_name: names[0],
         full_name: names.join(' '),
         deputy_id: 36,
-        okrug: nil,
-        photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
+        #okrug: nil,
+        #photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
         faction: "Українське об'єднання патріотів - УКРОП",
         start_date: "2015-10-20",
         end_date:  nil,
         created_at: "9999-12-31",
         updated_at: "9999-12-31"
     )
+    p.update(okrug: 19, photo_url: "http://dubno-adm.rv.ua/UserFiles/depytat/momotjukjv.jpg")
   end
   def create_mps_3
     names = %w{Опалак Вадим Олександрович}
-    People.first_or_create(
+    p = People.first_or_create(
         first_name: names[1],
         middle_name: names[2],
         last_name: names[0],
@@ -91,23 +93,25 @@ class ScrapeMp
         created_at: "9999-12-31",
         updated_at: "9999-12-31"
     )
+    p.update(photo_url: "http://dubno-adm.rv.ua/UserFiles/depytat/opalakvo.jpg")
   end
   def create_mps_4
     names = %w{Тимрук Віктор Станіславович}
-    People.first_or_create(
+    p = People.first_or_create(
         first_name: names[1],
         middle_name: names[2],
         last_name: names[0],
         full_name: names.join(' '),
         deputy_id: 38,
         okrug: 29,
-        photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
+        #photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
         faction: "Українське об'єднання патріотів - УКРОП",
         start_date: "2015-10-20",
         end_date:  nil,
         created_at: "9999-12-31",
         updated_at: "9999-12-31"
     )
+    p.update(photo_url: "http://dubno-adm.rv.ua/UserFiles/depytat/tymrukvs.jpg")
   end
   def scrape_mp(mp)
 
@@ -123,7 +127,7 @@ class ScrapeMp
        first_name = full_names[1]
        middle_name = full_names[2]
      end
-     faction = mp.last.nil? ? "Позафракційний" : mp.last.gsub(/Фракція/, "").strip
+     faction = mp[9]=="n/a" ? "Позафракційний" : mp[9].gsub(/Фракція/, "").strip
 
      people = People.first(
          deputy_id: deputy_id,
@@ -132,25 +136,26 @@ class ScrapeMp
          last_name: last_name,
          full_name: last_name + " " + first_name  + " " +  middle_name,
          start_date: "2015-10-20",
-         okrug: nil,
-         photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
+         #okrug: nil,
+         #photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
          faction: faction
      )
 
      unless people.nil?
-      people.update(end_date:  nil,  updated_at: Time.now)
+      people.update(end_date:  nil,  updated_at: Time.now, okrug: mp[6], photo_url: mp[10] )
      else
-        People.create(
+        p = People.create(
             deputy_id: deputy_id,
             first_name: first_name,
             middle_name: middle_name,
             last_name: last_name,
             full_name: last_name + " " + first_name  + " " +  middle_name,
             start_date: "2015-10-20",
-            okrug: nil,
-            photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
+            #okrug: nil,
+            #photo_url: "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif",
             faction: faction
         )
+        p.update(end_date:  nil,  updated_at: Time.now, okrug: mp[6], photo_url: mp[10] )
      end
   end
 end
